fe81aaabf86bb664206d4b5cfcd6cfec9c001535
Pedro Kawano <pedrokawano@outlook.com>
2024-05-01 21:59:46 -0300

    adiciona classe tokenservice

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog;
+
+public static class Configuration
+{
+    // TOKEN - JWT - Json Web Token
+    public static string JwtKey { get; set; } = "NDVmNTIwYjYtNDUzMS00YTUxLWIzNjctNjc3NzZkMGQzYjBj";
+}
